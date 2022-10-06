1ef62cce7e8e3b3252f3698c893bd630c83d5b0f
TimonClaeys <timon.claeys@outlook.com>
2022-10-06 13:08:32 +0200

    nieuwe layout toegevoegd

---
+++ test folder/main.js
@@ -0,0 +1,8 @@
+// import Map from 'ol/Map';
+// import MousePosition from 'ol/control/MousePosition';
+// import OSM from 'ol/source/OSM';
+// import TileLayer from 'ol/layer/Tile';
+// import View from 'ol/View';
+// import { createStringXY } from 'ol/coordinate';
+// import { defaults as defaultControls } from 'ol/control';
+
