af600c8ada380c038342c3aac721b64c775872ee
TimonClaeys <timon.claeys@outlook.com>
2022-10-06 15:38:27 +0200

    shit gefixt

--- java script/pswCheck.js
+++ java script/pswCheck.js
@@ -1,16 +1,15 @@
-var event = document.getElementById('psw-repeat')
-var password = document.getElementById('psw')
 
-function checkPsw(){
-    if(password.value === event.value)
-    {
+
+function checkPsw() {
+    var event = document.getElementById('psw-repeat')
+    var password = document.getElementById('psw')
+    if (password.value === event.value) {
         document.getElementById('pswR').style.color = 'black';
         document.getElementById('buttonSignup').style.visibility = 'unset';
         document.getElementById('form').setAttribute('action', "insert.php");
         document.getElementById('form').setAttribute('method', "post");
     }
-    else
-    {
+    else {
         document.getElementById('pswR').style.color = 'red';
         document.getElementById('pswR').innerHTML = 'Repeat Password (Passwords dont match)'
         document.getElementById('buttonSignup').style.visibility = 'hidden';
