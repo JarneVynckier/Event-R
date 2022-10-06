4dc898e852f6b52db0bcf04d6bd32d3e6d197f73
TimonClaeys <timon.claeys@outlook.com>
2022-10-06 15:02:48 +0200

    fixed scrolling

--- java script/script.js
+++ java script/script.js
@@ -1,20 +1,25 @@
 var popUp = document.getElementById('popUp');
 var register = document.getElementById('register')
+var body = document.getElementsByTagName("BODY")[0]; 
 
 register.onclick = function() {
     popUp.style.display = "flex";
+    body.style.overflow ="hidden";
+
 }
 
 var spanClose = document.getElementById('spanClose')
 
 spanClose.onclick = function (){
     popUp.style.display = "none";
+    body.style.overflow ="visible";
 }
 
 var buttonCancel = document.getElementById('buttonCancel')
 
 buttonCancel.onclick = function (){
     popUp.style.display = "none";
+    body.style.overflow = "visible";
 }
 
 // window.onclick = function(event) {
