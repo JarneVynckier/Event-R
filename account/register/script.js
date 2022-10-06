c73933a0f72e55039b4004a5b3a4450bb1daeba2
JarneVynckier <jarne.vynckier@student.odisse.be>
2022-10-06 13:23:02 +0200

    signUp with database and php

---
+++ account/register/script.js
@@ -0,0 +1,24 @@
+var popUp = document.getElementById('popUp');
+var register = document.getElementById('register')
+
+register.onclick = function() {
+    popUp.style.display = "flex";
+}
+
+var spanClose = document.getElementById('spanClose')
+
+spanClose.onclick = function (){
+    popUp.style.display = "none";
+}
+
+var buttonCancel = document.getElementById('buttonCancel')
+
+buttonCancel.onclick = function (){
+    popUp.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === popUp) {
+        popUp.style.display = "none";
+    }
+}
