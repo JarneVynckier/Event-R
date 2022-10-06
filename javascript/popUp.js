0354ffd7c8a89ad6c51d0ed407f7a593232934ca
JarneVynckier <jarne.vynckier@student.odisse.be>
2022-10-06 22:52:49 +0200

    code cleanup

--- javascript/popUp.js
+++ javascript/popUp.js
@@ -1,4 +1,3 @@
-
 var popUp = document.getElementById('popUp');
 var register = document.getElementById('register')
 var body = document.getElementsByTagName("BODY")[0];
@@ -32,20 +31,20 @@ buttonCancel.onclick = function (){
 function checkPsw() {
     var event = document.getElementById('psw-repeat')
     var password = document.getElementById('psw')
+
     if (password.value === event.value) {
         if (password.value === "") {
             document.getElementById('buttonSignup').style.visibility = 'hidden';
             return;
         }
+
         document.getElementById('pswR').style.color = 'black';
         document.getElementById('buttonSignup').style.visibility = 'unset';
         document.getElementById('form').setAttribute('action', "insert.php");
         document.getElementById('form').setAttribute('method', "post");
-
     }
     else {
         document.getElementById('pswR').style.color = 'red';
-        document.getElementById('pswR').innerHTML = 'Repeat Password (Passwords dont match)'
-
+        document.getElementById('pswR').innerHTML = 'Repeat Password (Passwords dont match)';
     }
 }
