555a61d5bfe4ca75e385d16392c3736dd9c0b68c
Gunnar <gunnar.deyne@student.odisee.be>
2022-11-09 11:28:01 +0100

    chatbot

--- index.php
+++ index.php
@@ -302,7 +302,17 @@ if ($send) {
         <footer>
 
         </footer>
-
+    <!-- Start of ChatBot (www.chatbot.com) code -->
+    <script type="text/javascript">
+        window.__be = window.__be || {};
+        window.__be.id = "63695f76ea2b2800076e0094";
+        (function() {
+            var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+            be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+        })();
+    </script>
+    <!-- End of ChatBot code -->
     </body>
 
 </html>
